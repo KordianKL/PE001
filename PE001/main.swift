fcdaf724ed029ef089a1c85bbe79f0cc06b1fab9
Kordian Ledzion <kkordikk@gmail.com>
2017-04-11 18:17:01 +0200

    Some benchmarking

--- PE001/main.swift
+++ PE001/main.swift
@@ -8,15 +8,213 @@
 
 import Foundation
 
-var result = 8
-DispatchQueue.global(qos: .userInitiated).async {
+var times = [Double]()
+
+var index = 0
+
+func printTimeElapsedWhenRunningCode(operation:()->()) {
+    let startTime = CFAbsoluteTimeGetCurrent()
+    operation()
+    let timeElapsed = CFAbsoluteTimeGetCurrent() - startTime
+    print("#\(index) \(timeElapsed)")
+    index += 1
+    times.append(Double(timeElapsed))
+}
+
+print("---- ---- ITER ---- ----")
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
     var i = 6
     while(i < 1000){
         if(i % 3 == 0 || i % 5 == 0){
             result += i
         }
-    i += 1
+        i += 1
     }
 }
 
-print(result)
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    var i = 6
+    while(i < 1000){
+        if(i % 3 == 0 || i % 5 == 0){
+            result += i
+        }
+        i += 1
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    var i = 6
+    while(i < 1000){
+        if(i % 3 == 0 || i % 5 == 0){
+            result += i
+        }
+        i += 1
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    var i = 6
+    while(i < 1000){
+        if(i % 3 == 0 || i % 5 == 0){
+            result += i
+        }
+        i += 1
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    var i = 6
+    while(i < 1000){
+        if(i % 3 == 0 || i % 5 == 0){
+            result += i
+        }
+        i += 1
+    }
+}
+
+print("---- ---- SYNC ---- ----")
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).sync {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).sync {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).sync {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).sync {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).sync {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+print("---- ---- ASYNC ---- ----")
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).async {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).async {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).async {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).async {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+printTimeElapsedWhenRunningCode{
+    var result = 8
+    DispatchQueue.global(qos: .userInteractive).async {
+        var i = 6
+        while(i < 1000){
+            if(i % 3 == 0 || i % 5 == 0){
+                result += i
+            }
+            i += 1
+        }
+    }
+}
+
+print("---- ---- RESULT ---- ----")
+
+//delay to let the async wokr finish
+usleep(100)
+
+print("\(times.min()!) at index \(times.index(of: times.min()!)!)")
